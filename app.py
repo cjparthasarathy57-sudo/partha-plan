a24b3e5432d12e5dd6f165101d811ee48f6d63d4
cjparthasarathy57-sudo <cjparthasarathy57@gmail.com>
2025-10-02 08:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="static", static_url_path="")
+
+@app.route("/")
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import cv2
@@ -182,3 +189,4 @@ def search_plans():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
